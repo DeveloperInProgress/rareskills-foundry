1e0a9c9fdf6d2eca2748a7198096ed60726c6fc6
Naveen V <velnaveen99@gmail.com>
2022-10-30 09:48:12 +0000

    use bancors formula in bonding curve

--- test/TokenSaleWithBondingCurve.sol
+++ test/TokenSaleWithBondingCurve.sol
@@ -22,42 +22,26 @@ contract TokenSaleWithBondingCurveTest is Test {
     }
 
     function test_buyTokens() public {
-        uint256 tokenPrice = sale.tokenPrice();
+        uint256 purchaseReturn = sale.computePurchaseReturns(1e18);
         hoax(alice, 1e18);
         
         sale.buyTokens{value: 1e18}();
         uint256 balance = sale.balanceOf(alice);
-        uint256 expected = tokenPrice.mul(1e18);
 
-        assertEq(balance, expected);
-
-        uint256 priceApres = sale.tokenPrice();
-        uint256 priceExpected = 1+1e18;
-
-        assertEq(priceApres, priceExpected);
+        assertEq(balance, purchaseReturn);
     }
 
     function test_sellTokens() public {
-        uint256 amountToSell = 100e18;
-        sale.mint(alice, amountToSell);
-        deal(address(sale), amountToSell); //add eth balance to contract
+        uint256 purchaseReturn = sale.computePurchaseReturns(100e18);
+        startHoax(alice, 100e18);
+        sale.buyTokens{value: 100e18}();
         
-        uint256 price = sale.tokenPrice();
-        uint256 ethToSend = (amountToSell).div(price);
-        uint256 fee = ethToSend.div(10);
-        ethToSend = ethToSend.sub(fee);
+        uint256 saleReturn = sale.computeSaleReturns(purchaseReturn/2);
+        uint256 ethToSend = sale.sellTokens(purchaseReturn/2);
         
-        vm.prank(alice);
-        sale.sellTokens(amountToSell);
-        uint256 balance = alice.balance;
-        
-        assertEq(ethToSend, balance);
-        assertEq(address(sale).balance, amountToSell.sub(ethToSend));
-    
-        uint256 priceApres = sale.tokenPrice();
-        uint256 priceExpected = 1; // All minted tokens were sold
-
-        assertEq(priceExpected, priceApres);
+        uint256 saleReturnAfterFee = saleReturn - saleReturn.div(10);
+        assertEq(ethToSend, saleReturnAfterFee);
+        assertEq(alice.balance, ethToSend);
     }   
 
     function testRevert_SellTokensNotEnoughBalance() public {
