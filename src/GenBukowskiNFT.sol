0e0bb648d3ae51534292343d3cc350f7b44158e2
Naveen V <velnaveen99@gmail.com>
2022-11-01 12:38:16 +0000

    add GenBukowskiNFT

---
+++ src/GenBukowskiNFT.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.13;
+
+import "openzeppelin-contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/utils/Strings.sol";
+
+contract GenBukowskiNFT is ERC721 {
+
+    using Strings for uint256;
+
+    uint256 public tokenSupply = 0;
+    uint256 public constant MAX_SUPPLY = 10;
+    uint256 public constant MINT_COST = 0;
+    
+    address immutable deployer;
+
+    modifier onlyDeployer {
+        require(deployer == msg.sender, "Caller is not the owner");
+        _;
+    }
+
+    constructor() ERC721("Gen Bukowski", "GBW") {
+        deployer = msg.sender;
+    }
+
+
+    function mint() external payable {
+        require(tokenSupply < MAX_SUPPLY, "No more tokens availabel");
+        require(msg.value == MINT_COST, "Incorrect price");
+        
+        _mint(msg.sender, tokenSupply);
+        tokenSupply++;
+    }
+
+    function viewBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmcU4C8mdCa3vTi5SKgUnSzKBRzFitPyxqtJjqj3rPHDyZ";
+    }
+
+    function withdraw() external onlyDeployer {
+        payable(deployer).transfer(address(this).balance);
+    }
+
+}
